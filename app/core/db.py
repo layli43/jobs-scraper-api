4452ead3b232453c0d2c0d08556ee9934f021595
layli43 <layliuanyu@gmail.com>
2026-09-22 21:26:19 +0100

    [mod]:Databaseconnection safety guarantee

--- app/core/db.py
+++ app/core/db.py
@@ -1,8 +1,17 @@
 from sqlmodel import create_engine
+import logging
 
 from app.core.config import settings
 
-engine = create_engine(str(settings.DATABASE_URL), pool_pre_ping=True)
+logger = logging.getLogger(__name__)
+
+DATABASE_URL = settings.DATABASE_URL
+
+if DATABASE_URL:
+    engine = create_engine(str(settings.DATABASE_URL), pool_pre_ping=True)
+else:
+    logger.warning("No DATABASE_URL configured, can't connect to database")
+    engine = None
 
 
 # make sure all SQLModel models are imported (app.models) before initializing DB
